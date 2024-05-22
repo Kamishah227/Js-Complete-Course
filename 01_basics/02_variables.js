89680c2b1a4adea10b368d6f7887c83be511fe08
Kamishah227 <35210153+Kamishah227@users.noreply.github.com>
2024-05-22 16:26:34 +0000

    variables understanding

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,15 @@
+const accountId=1234
+let accountEmail="kamishah@gmail.com"
+var accountPass="Saving"
+var accountTitle="Saving"
+acountCity="Sailkot"
+let accountstatus=false;
+
+
+// accountId=2 Not Allowed (values of Constents can't be changed)
+
+accountEmail="kamishah353@gmail.com"
+accountPass="kami"
+accountTitle="Current"
+console.table([accountEmail,accountPass,accountTitle,accountstatus]);
+
